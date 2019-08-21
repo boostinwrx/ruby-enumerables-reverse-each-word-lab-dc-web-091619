38daca1d4bedcfec33ea912f180765c44f8bad54
Antonio Abraham <tony.calablava@gmail.com>
2019-08-21 01:19:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(s ="This is a sentence")
   array = []
-array = s.split(" ").collect{|w|return"#{w.each}"+}
+array = s.split(" ").collect{|w|return"#{w.each}"}
 }
 return array
 end
